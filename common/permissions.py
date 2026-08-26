17f4c2a246257d8ae7ce1f7ea681e425a831a914
Priyanka-ch-dev <Priyankachannappahadapad@gmail.com>
2026-08-26 14:51:35 +0530

    user permission

--- common/permissions.py
+++ common/permissions.py
@@ -11,13 +11,13 @@ class IsSubAdmin(BasePermission):
 
 class IsCashier(BasePermission):
     def has_permission(self, request, view):
-        # Admins, Sub-Admins, and Cashiers can access billing
-        return bool(request.user and request.user.is_authenticated and request.user.role in [ROLE_ADMIN, ROLE_SUB_ADMIN, ROLE_CASHIER])
+        # Admins, Sub-Admins, Cashiers, and Store Staff can access billing
+        return bool(request.user and request.user.is_authenticated and request.user.role in [ROLE_ADMIN, ROLE_SUB_ADMIN, ROLE_CASHIER, ROLE_STORE_STAFF])
 
 class IsStoreStaff(BasePermission):
     def has_permission(self, request, view):
-        # Admins, Sub-Admins, and Store Staff can access inventory/products
-        return bool(request.user and request.user.is_authenticated and request.user.role in [ROLE_ADMIN, ROLE_SUB_ADMIN, ROLE_STORE_STAFF])
+        # Admins, Sub-Admins, Store Staff, and Cashiers can access inventory/products
+        return bool(request.user and request.user.is_authenticated and request.user.role in [ROLE_ADMIN, ROLE_SUB_ADMIN, ROLE_STORE_STAFF, ROLE_CASHIER])
 
 class IsBranchStaff(BasePermission):
     def has_permission(self, request, view):
